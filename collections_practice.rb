a012d433f5d96b62ef1765223d32c525e6af48b5
Josh Raiborde <joshsrai@yahoo.com>
2020-07-30 12:12:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort {|a, b| -1 * (a<=>b)}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements_from_to(array, from, to)
+  temp = array[from]
+  array[from] = array[to]
+  array[to] = temp
+  array
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 1,2)
+end
+
+def reverse_array(array)
+  reverse =[]
+  i = array.length - 1
+  while i >= 0
+  reverse << array[i]
+  i -= 1
+    end
+    reverse
+end
 
