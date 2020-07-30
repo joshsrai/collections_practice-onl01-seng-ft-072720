bb0d6076fd94b8675670240c888076741f674cdb
Josh Raiborde <joshsrai@yahoo.com>
2020-07-30 13:32:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,3 +48,13 @@ end
 def sum_array(array)
   array.inject{|sum, i| sum += i }
 end
+
+def add_s(array)
+  array.each_with_index.collect do |string,index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end
